110db9b40597fb3341980cdd93aa256fd0b4e36e
Luigi-bot37 <143562859+Luigi-bot37@users.noreply.github.com>
2023-10-27 15:17:07 -0600

    Add files via upload

---
+++ EVA2_14_CICLO_FOR/src/eva2_14_ciclo_for/EVA2_14_CICLO_FOR.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_ciclo_for;
+
+/**
+ *
+ * @author selva
+ */
+public class EVA2_14_CICLO_FOR {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //for---> desde
+        //se ejecuta desde un punto a otro
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("---------------");
+        for (int i = 9; i >=0; i--) 
+            System.out.println(i);
+        System.out.println("---------------");
+        //NUMEROS PARES DE 0 a 100
+        for (int i = 0; i <=100; i+=2) 
+             System.out.print(i+ " - ");
+        
+           
+        
+        
+    }
+    
+}
